92ec76edadb34e86626bc4decb9bbae62728f3b9
Anubhav Vats <anubhav.vats@postman.com>
2023-10-31 20:35:51 +0530

    Suffix image processor architecture in tag

--- microservices/city-explorer/scripts/deploy.sh
+++ microservices/city-explorer/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION='1.0.0'
+VERSION='1.0.0-amd64'
 docker buildx build -t eronbituchenik/cityexplorer:$VERSION  --platform linux/amd64 .
 docker push eronbituchenik/cityexplorer:$VERSION
