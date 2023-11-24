d0abe2b5555b5408f7f9667bbcade31d3b281bc3
Anubhav Vats <anubhav.vats@postman.com>
2023-11-24 06:30:04 +0530

    feat: update the docker image version

--- microservices/city-explorer/scripts/deploy.sh
+++ microservices/city-explorer/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION='1.0.0-amd64'
+VERSION='1.0.1-amd64'
 docker buildx build -t eronbituchenik/cityexplorer:$VERSION  --platform linux/amd64 .
 docker push eronbituchenik/cityexplorer:$VERSION
